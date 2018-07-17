6aebecdc5a6e24f9e3d1f73a7ca8d1b20391c88e
Tonmoy Sardar <tonmoy.1984@gmail.com>
2018-07-17 11:05:46 +0530

    update

--- src/app/layout/app-success/app-success.component.ts
+++ src/app/layout/app-success/app-success.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
-
+import { } from '@types/googlemaps';
 import { ViewChild, ElementRef, NgZone } from '@angular/core';
 
 @Component({
