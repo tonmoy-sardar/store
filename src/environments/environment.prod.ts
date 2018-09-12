d82c65b0da336bd88782965874228717f0bbf2e5
Tonmoy Sardar <tonmoy.1984@gmail.com>
2018-09-12 10:21:38 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://www.api-store.banaoapp.com:8001/',
-  urlEndpoint: 'http://www.api-store.banaoapp.com:8001',
+  apiEndpoint: 'https://www.api-store.banaoapp.com:8002/',
+  urlEndpoint: 'http://www.api-store.banaoapp.com:8002',
 };
