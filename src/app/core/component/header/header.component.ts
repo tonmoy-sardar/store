7ff435dc6d42c9269a67fea558ad822af8d7453b
Tonmoy Sardar <tonmoy.1984@gmail.com>
2018-09-13 17:41:43 +0530

    QA fixing

--- src/app/core/component/header/header.component.ts
+++ src/app/core/component/header/header.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material';
 import { LoginComponent } from '../login/login.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { CreateAppService } from '../../services/create-app.service';
+
 
 @Component({
   selector: 'app-header',
@@ -11,23 +13,28 @@ import { LoginService } from '../../services/login.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public dialog: MatDialog,private router: Router, private loginService: LoginService) {
+  constructor(
+    public dialog: MatDialog,
+    private router: Router,
+    private loginService: LoginService,
+    private createAppService: CreateAppService
+  ) {
 
     loginService.getLoggedInStatus.subscribe(status => this.changeStatus(status));
-   }
+  }
   isLoggedin: boolean;
   user_name: string;
   user_group: string = '';
 
   loginStatus: boolean;
-
+  userAndAppDetails: any;
+  app_exist_key: boolean;
   private changeStatus(status: boolean): void {
     this.loginStatus = status;
-    if(this.loginStatus == true)
-    {
+    if (this.loginStatus == true) {
       this.ngOnInit();
     }
-    
+
     //alert(this.userName)
   }
 
@@ -35,11 +42,29 @@ export class HeaderComponent implements OnInit {
     if (localStorage.getItem('isLoggedin')) {
       this.isLoggedin = true;
       this.user_name = localStorage.getItem('logedUserFirstName');
-      if(localStorage.getItem('logedUserUserGroup')){
+      if (localStorage.getItem('logedUserUserGroup')) {
         this.user_group = localStorage.getItem('logedUserUserGroup')
       }
+      this.getAppAnduserDetailsByUserID(localStorage.getItem('logedUserUserId'))
     }
   }
+
+  getAppAnduserDetailsByUserID(id) {
+    this.createAppService.getAppAnduserDetailsByUserID(id).subscribe(
+      (data) => {
+        console.log(data);
+        this.userAndAppDetails = data;
+        if (data.user_details.length > 0) {
+          if (data.user_details[0].app_details.length > 0) {
+            this.app_exist_key = true;
+          }
+        }
+      },
+      error => {
+
+      }
+    );
+  };
 
   openLogin() {
     this.dialog.open(
