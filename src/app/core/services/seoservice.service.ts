7b6c5bc465342112221f84b33e4a11720d6cf72b
Biswajit Manna <biswajit.manna@shyamfuture.com>
2018-12-24 15:14:50 +0530

    social share working

--- src/app/core/services/seoservice.service.ts
+++ src/app/core/services/seoservice.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable()
 export class SeoserviceService {
 
-    constructor(private title: Title, private meta: Meta) { }
+    constructor(private title: Title, private meta: Meta, @Inject(DOCUMENT) private doc) { }
 
 
     updateTitle(title: string) {
@@ -19,12 +20,22 @@ export class SeoserviceService {
         this.meta.updateTag({ name: 'keywords', content: category })
     }
 
-    updateOg(data: any){
+    updateOg(data: any) {
+        // og
         this.meta.updateTag({ property: 'og:type', content: 'Website' })
         this.meta.updateTag({ property: 'og:image', content: data['og_image'] })
         this.meta.updateTag({ property: 'og:title', content: data['og_title'] })
         this.meta.updateTag({ property: 'og:description', content: data['og_description'] })
         this.meta.updateTag({ property: 'og:url', content: data['og_url'] })
+        // canonical
+        this.setCanonical();
+    }
+
+    setCanonical() {
+        let link: HTMLLinkElement = this.doc.createElement('link');
+        link.setAttribute('rel', 'canonical');
+        this.doc.head.appendChild(link);
+        link.setAttribute('href', this.doc.URL);
     }
 
 }
